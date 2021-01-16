8adc6871162917c8405b2aadd3b4aa1aa9fb7a23
blurryfacebeat <blurryfacebeat@gmail.com>
2021-01-16 17:25:31 +0700

    Make hero, how we work, boost, make responsive

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Project Setup');
+// Работа с burger-menu
+const burger_menu = document.querySelector('.header__menu');
+const site_nav = document.querySelector('.nav__items');
+
+// window.addEventListener('resize', () => {
+//   if (document.documentElement.clientWidth <= 860) {
+//     site_nav.classList.add('hidden');
+//   } else {
+//     site_nav.classList.remove('hidden');
+//   }
+// });
+
+burger_menu.addEventListener('click', () => {
+  site_nav.classList.toggle('show-flex');
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log('Project Setup');
+// Работа с burger-menu
+var burger_menu = document.querySelector('.header__menu');
+var site_nav = document.querySelector('.nav__items'); // window.addEventListener('resize', () => {
+//   if (document.documentElement.clientWidth <= 860) {
+//     site_nav.classList.add('hidden');
+//   } else {
+//     site_nav.classList.remove('hidden');
+//   }
+// });
+
+burger_menu.addEventListener('click', function () {
+  site_nav.classList.toggle('show-flex');
+});
