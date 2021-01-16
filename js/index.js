1c9433a142531db06bd0e3e567235e9b3848c147
blurryfacebeat <blurryfacebeat@gmail.com>
2021-01-17 03:51:39 +0700

    End

--- js/index.js
+++ js/index.js
@@ -2,14 +2,7 @@
 
 // Работа с burger-menu
 var burger_menu = document.querySelector('.header__menu');
-var site_nav = document.querySelector('.nav__items'); // window.addEventListener('resize', () => {
-//   if (document.documentElement.clientWidth <= 860) {
-//     site_nav.classList.add('hidden');
-//   } else {
-//     site_nav.classList.remove('hidden');
-//   }
-// });
-
+var site_nav = document.querySelector('.nav__items');
 burger_menu.addEventListener('click', function () {
   site_nav.classList.toggle('show-flex');
 });
